fa7aed84784b7f3ca5426423d3e0affb5e8a04d8
Adrian <a.podlasinski@icloud.com>
2024-03-19 16:38:35 +0100

    user item z kolorami

--- src/UserItem.js
+++ src/UserItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 function UserItem({ user }) {
+    const userItemStyle = {
+        color: 'orange', 
+        fontWeight: 'bold' 
+        
+      };
   return (
-    <div>
+    <div style={userItemStyle}>
       <h3>{user.name}</h3>
       <p>Email: {user.email}</p>
       <p>UserName: {user.username}</p>
+      <p>Phone: {user.phone}</p>
     </div>
   );
 }
