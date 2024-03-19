81078f6082297d3cf3379d4545dfe161e75173fb
Adrian <a.podlasinski@icloud.com>
2024-03-19 17:42:57 +0100

    UserItem1

--- src/UserItem.js
+++ src/UserItem.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 function UserItem({ user }) {
     const userItemStyle = {
-        color: 'orange', 
-        fontWeight: 'bold' 
+        color: 'red', 
+        fontWeight: 'bold',
+        border: '1px dashed orange' 
+
         
       };
   return (
     <div style={userItemStyle}>
       <h3>{user.name}</h3>
       <p>Email: {user.email}</p>
-      <p>UserName: {user.username}</p>
+      <p>User Name: {user.username}</p>
       <p>Phone: {user.phone}</p>
+      <p>Adress: {user.address.street} City: {user.address.city}</p>
     </div>
   );
 }
